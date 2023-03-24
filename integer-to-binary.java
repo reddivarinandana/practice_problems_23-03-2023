9cfb3693fe7b4c2147bebadc539b16f97c49556e
reddivarinandana <nadanareddy1122@gmail.com>
2023-03-24 10:50:02 +0530

    practice-problems

--- integer-to-binary.java
+++ integer-to-binary.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-       int value = 92;
-       System.out.println("Integer: "+ value);
-       System.out.println("Binary : " + Integer.toBinaryString(value));
+       int a = 92;
+       System.out.println(Integer.toBinaryString(a));
     }
 }
